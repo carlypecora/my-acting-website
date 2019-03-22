7e2cb0308596e1b2741cfe9b1c9f944a051ce6b4
Carly Pecora <carly@carlypecora.com>
2019-03-22 09:07:47 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Carly Pecora's Acting Website
+This is the source code for my personal, acting website. This is a *work in progress*! There is still a lot of cruft and code cleanup to be done--but it was my first website!
---
+++ src/actingSite.js
@@ -0,0 +1,30 @@
+var slideIndex = 0;
+showSlides();
+mapNavToPage();
+
+function showSlides() {
+  var slides = document.getElementsByClassName("mySlides");
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none"; 
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  	slides[slideIndex-1].style.display = "block"; 
+  	setTimeout(showSlides, 6000); 
+}
+
+
+
+
+function mapNavToPage(){
+	$(".resume").on("click", function(){
+		document.location.assign("file:///Users/carlypecora/Documents/actingSite/res.html");
+	});
+	$(".portfolio").on("click", function(){
+		document.location.assign("file:///Users/carlypecora/Documents/actingSite/portfolio.html");
+	})
+	$(".navbar-brand").on("click", function(){
+		document.location.assign("file:///Users/carlypecora/Documents/actingSite/actingSite.html");
+	})
+
+}
